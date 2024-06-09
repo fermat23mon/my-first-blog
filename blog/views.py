91c58b0cf6866be90482dea723845ff3342dc031
megadanimon <danidex9618@gmail.com>
2024-06-08 23:26:27 -0500

    Se supone ya sirve

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,8 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
+
     posts = Post.objects.all()
+    
     return render(request, 'blog/post_list.html', {'posts': posts})
+    

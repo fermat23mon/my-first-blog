e6c4d5a72cb376fac9696c9b2d4a79f4859cd5aa
megadanimon <danidex9618@gmail.com>
2024-06-08 22:21:22 -0500

    Cambio de posteos a posts

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posteos = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posteos': posteos})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})

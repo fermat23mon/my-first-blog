c9abbe1ae14e849794c46e439362b7d02d2e14e5
megadanimon <danidex9618@gmail.com>
2024-07-27 20:05:28 -0500

    ayudame senol

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-
     posts = Post.objects.all()
-    
     return render(request, 'blog/post_list.html', {'posts': posts})
-    
